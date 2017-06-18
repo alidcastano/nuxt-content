8308fcc26f92d737648f40203725f6b20a4e4008
Alid Castano <alidcastano@gmail.com>
2017-06-18 16:28:04 -0400

    made sure registered directories that are nested are not repeated

--- index.js
+++ index.js
@@ -1,57 +1,10 @@
+require('vue-content-loader')
 const join = require('path').join
 const fs = require('fs')
 const fm = require('front-matter')
 const pathToRegexp = require('path-to-regexp')
 const paramCase = require('param-case')
 
-/**
- * Nuxt Content Module.
- *
- *  Module Options:
- *   - srcDir    : STRING - content source directory
- *   - routePath : STRING - content parent route
- *   - content   : ARRAY  - content type and configuation options
- *
- * Content Options:
- *   - slug      : STRING  - content url identifation name
- *   - permalink : STRING  - content url path options
- *   - isPost    : BOOLEAN - whether the content is chronological
- *
- *
- * Content configuration can be done via modules or content property.
- *    @note All paths are relative to Nuxt root directory.
- *
- *  example:
- *    modules: [
- *      [@nuxtjs/content, {
- *        srcDir: "content",
- *        routePath: "blog"
- *        content: ["posts", { permalink: ":title" }]
- *      }]
- *    ]
- *   OR
- *   modules: [@nuxtjs/content, { srcDir: "content"} ]
- *   content: ["posts", { permalink: ":title" }]
- *
- *
- * For content, 2D Array is allowed multiple content types:
- *
- * example:
- *   content: [
- *    ["posts", { permalink: ":year/:title" }],
- *    ["collection", { permalink: ":year/:title", isPost: false }]
- *   ]
- *
- *  Route Configuration:
-  *  Can either be top level route ("/") or nested route under an existing nuxt page.
-  *    @note There cannot exist any dynamic route at same level of content route.
-  *
-  *  example: [
-  *    ["posts", { routePath: "/" }],          // posts/HelloWorld -> /Helloworld
-  *    ["projects", { routePath: "projects" }] // projects/Nuxt -> projects/Nuxt
-  *   ]
- *
- */
 module.exports = function (options) {
   const nuxt = this.options
 
@@ -64,81 +17,86 @@ module.exports = function (options) {
     }]
   })
 
-  options.rootPath = options.srcDir
-    ? join(nuxt.rootDir, options.srcDir)
-    : config.rootDir
-
+  // create content config
+  const config = {}
+  config.options = options
+  config.options.rootPath = join(nuxt.rootDir, options.srcDir || "/")
+  config.content = nuxt.content || options.content || ['/']
+  if (!is2DArray(config.content)) config.content = [config.content]
 
   // create routes for registered content
-  const config = nuxt.content || options.content
   nuxt.router.extendRoutes = (routes, resolve) => {
-    if (Array.isArray(config[0])) { // nested array of content sources
-      config.forEach(type => {
-        const filesData = getFilesData(type[0], options.rootPath)
-        const contentOpts = getContentOpts(type[1], options)
-        addRoutes(contentOpts, routes, filesData)
-      })
-    } else { // single content source
-      const filesData = getFilesData(config[0], options.rootPath)
-      const contentOpts = getContentOpts(config[1], options)
-      addRoutes(contentOpts, routes, filesData)
-    }
+    const contentData = getContentData(config)
+    addRoutes(routes, contentData)
   }
 }
 
-/**
- * Get content options via 1) content property 2) module options
- */
-function getContentOpts (config, options) {
-  return {
-    route: join("/", config.routePath || options.routePath),
-    permalink: config.permalink || options.permalink,
-    isPost: !(config.isPost === false)
-  }
-}
 
 /**
- * Recursively get all content file paths with their respective metadata.
+ * Returns 2D array of content data, each with its respective
+ * registered directory, options, and nested files.
  */
-function getFilesData (contentDir, rootPath, filesData = []) {
-  const contentPath = join(rootPath, contentDir)
-  fs.readdirSync(contentPath).forEach((stat, opts) => {
-    const statPath = join(contentPath, stat)
-    if(fs.statSync(statPath).isDirectory()) {
-      const nestedContentDir = join(contentDir, stat)
-      getFilesData(nestedContentDir, rootPath, filesData)
-    } else {
-      filesData.push({
-        src: statPath,      // path to file
-        section: contentDir // path to file directory
-      })
-    }
-  })
-  return filesData
-}
+function getContentData (config) {
+  const rootPath = config.options.rootPath
+  const contentData = []
+  config.content.forEach(type => {
+    const dir = join('/', type[0])
+    const opts = getContentOpts(type[1] || {}, config.options)
+    const path = join(rootPath, dir)
+    const otherRegisteredDirs = getOtherRegisteredDirs(dir, config.content)
 
-/**
- * Adds content component to route.
- */
-function addRoutes(options, routes, filesData) {
-  if (options.route === "/") { // Top Level Route
-    filesData.forEach(file => {
-      routes.push(createRoute(file, options))
-    })
-  } else { // Nested route
-    routes.forEach(route => {
-      if (route.path.indexOf(options.route) > -1) {
-        if (!route.children) route.children = []
-        filesData.forEach(file => {
-          route.children.push(createRoute(file, options))
+    const filesData = []
+    fs.readdirSync(path).forEach(stat => {
+      const statPath = join(path, stat)
+      if(fs.statSync(statPath).isDirectory()) { // Nested Files
+        const dirSection = join(dir, stat)
+        if (!(otherRegisteredDirs.indexOf(dirSection) > -1)) {
+          filesData.push(...getFilesData(dir, rootPath, otherRegisteredDirs))
+        }
+      }
+      else { // Top Level file
+        filesData.push({
+          src: statPath,
+          section: dir
         })
       }
     })
-  }
+    contentData.push([opts, filesData])
+  })
+  return contentData
 }
 
+
 /**
- * Gets page data via via 1) front-matter 2) file name.
+ * Creates top level or nested route for each content file.
+ */
+function addRoutes(routes, contentData) {
+  contentData.forEach(type => {
+    const opts = type[0]
+    const filesData = type[1]
+
+    if (opts.route === "/") { // Top level route
+      filesData.forEach(file => {
+        routes.push(createRoute(file, opts))
+      })
+    } else { // Nested route
+      routes.forEach(route => {
+        const isRoute = route.path.indexOf(opts.route) > -1
+        if (isRoute) {
+          if (!route.children) route.children = []
+          filesData.forEach(file => {
+            route.children.push(createRoute(file, opts))
+          })
+        }
+      })
+    }
+  })
+  console.log(routes)
+}
+
+
+/** // TODO /%2Fposts%2Fregistered/some-advice
+ * Gets page data via 1) front-matter 2) file name.
  */
 function createRoute (file, options) {
   const fileSource = fs.readFileSync(file.src, 'utf-8')
@@ -168,6 +126,17 @@ function createRoute (file, options) {
   }
 }
 
+/**
+ * Gets content options via 1) content config property 2) module Options 3) defaults
+ */
+function getContentOpts (config, options) {
+  const route = config.routePath || options.routePath || config.srcDir || ''
+  return {
+    route: join('/', route),
+    permalink: config.permalink || options.permalink || ':slug',
+    isPost: !(config.isPost === false)
+  }
+}
 
 
 /**
@@ -177,4 +146,44 @@ function toSlug (fileName) {
   const date = /!?(\d{4}-\d{2}-\d{2}-)/
   const ext = /(.)[^.]+$/
   return fileName.replace(date, '').replace(ext, '')
+}
+
+/** // TODO still repeating
+ * Recursively get all content files with their respective metadata.
+ */
+function getFilesData (contentDir, rootPath, blacklist, filesData = []) {
+  const contentPath = join(rootPath, contentDir)
+
+  fs.readdirSync(contentPath).forEach((stat, opts) => {
+    const statPath = join(contentPath, stat)
+    if(fs.statSync(statPath).isDirectory()) {
+      const nestedContentDir = join(contentDir, stat)
+      if (blacklist.indexOf(nestedContentDir) > -1) {
+        getFilesData(nestedContentDir, rootPath, blacklist, filesData)
+      }
+    } else {
+      filesData.push({
+        src: statPath,      // path to file
+        section: contentDir // path to file directory
+      })
+    }
+  })
+  return filesData
+}
+
+
+/**
+ * Gets all the registered content directory types.
+ */
+function  getOtherRegisteredDirs (currDir, contentTypes) {
+  const dirs = []
+  contentTypes.forEach(type => {
+    const dir = type[0]
+    if (dir !== currDir) dirs.push(join('/' + dir))
+  })
+  return dirs
+}
+
+function is2DArray (arr) {
+  return Array.isArray(arr[0])
 }
